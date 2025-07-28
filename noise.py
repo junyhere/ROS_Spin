c1f602fd848e331cadc5b8a26346db422cb4e42c
Arjun Lingala <junyyyhere@gmail.com>
2025-07-28 09:16:18 -0400

    Update noise.py

--- noise.py
+++ noise.py
@@ -1,8 +1,8 @@
-from qiskit_aer.noise import (NoiseModel, amplitude_damping_error, 
-                                        phase_damping_error)
+from qiskit_aer.noise import (NoiseModel, amplitude_damping_error, phase_damping_error)
 
 
 def noise_mod(gamma_amp: float, phi_frac: float = 0.0) -> NoiseModel:
+    """Create an amplitude and phase damping noise model"""
     gamma_amp = gamma_amp * (1.0 - phi_frac)
     gamma_phi = gamma_amp * phi_frac
     noise = NoiseModel()
@@ -11,4 +11,3 @@ def noise_mod(gamma_amp: float, phi_frac: float = 0.0) -> NoiseModel:
     if gamma_phi > 0:
         noise.add_all_qubit_quantum_error(phase_damping_error(gamma_phi), ["id"])
     return noise
-
