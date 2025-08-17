6146d247f3a95cc749f6f744065e8326311c8c7f
Arjun Lingala <junyyyhere@gmail.com>
2025-08-17 17:12:13 -0400

    Update noise.py

--- noise.py
+++ noise.py
@@ -6,8 +6,13 @@ def noise_mod(gamma_amp: float, phi_frac: float = 0.0) -> NoiseModel:
     gamma_amp = gamma_amp * (1.0 - phi_frac)
     gamma_phi = gamma_amp * phi_frac
     noise = NoiseModel()
+
+    error = None
     if gamma_amp > 0:
-        noise.add_all_qubit_quantum_error(amplitude_damping_error(gamma_amp), ["id"])
+        error = amplitude_damping_error(gamma_amp)
     if gamma_phi > 0:
-        noise.add_all_qubit_quantum_error(phase_damping_error(gamma_phi), ["id"])
+        phi_err = phase_damping_error(gamma_phi)
+        error = phi_err if error is None else error.compose(phi_err)
+    if error:
+        noise.add_all_qubit_quantum_error(error, ["id"])
     return noise
